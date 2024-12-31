4a00037e3852dae295a5b79893146ec5f8a3cd3f
CrazyMapleButter <bwlanctot@icloud.com>
2024-12-31 16:26:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // You can add any JavaScript here to enhance your site
     console.log("Commands page loaded!");
 
-    // Example: Highlighting a command when hovered
     const commandItems = document.querySelectorAll('.commands-list li');
     commandItems.forEach(item => {
         item.addEventListener('mouseover', () => {
